88a945c8091b8879e572c622438bc185e3758b06
Arthur Maia <arthur.maia.dev@gmail.com>
2020-11-15 19:18:17 -0300

    .

--- ChaveValorHandler.java
+++ ChaveValorHandler.java
@@ -6,15 +6,17 @@ import chavevalor.*;
 
 public class ChaveValorHandler implements ChaveValor.Iface {
   private HashMap<Integer,String> kv = new HashMap<>();
+  public String oldValue;
   @Override
   public String getKV(int key) throws TException {
-    if(kv.containsKey(key))
-      return kv.get(key);
+    if(kv.containsKey(key) && oldValue != null)
+      return oldValue;
     else
       throw new KeyNotFound();
   }
   @Override
   public boolean setKV(int key, String valor) throws TException {
+    oldValue = kv.get(key);
     kv.put(key,valor);
     return true;
   }
